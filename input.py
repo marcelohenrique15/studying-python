e7043fb92dc7612164dc7653159f812bad2e1f4c
Marcelo Henrique <83303964+marcelohenrique15@users.noreply.github.com>
2021-08-24 17:45:55 -0300

    Update input.py

--- input.py
+++ input.py
@@ -1,12 +1,9 @@
 nome_aluno = input("Digite seu nome e sobrenome:")
-idade_aluno = input("Digite sua idade:")
+idade_aluno = int(input("Digite sua idade:"))
 escola = input("Digite o nome de sua escola:")
-nota1 = input("Digite a sua nota da primeira unidade:")
-nota2 = input("Digite a sua nota da segunda unidade:")
-nota3 = input("Digite a sua nota da terceira unidade:")
-nota1 = float(nota1)
-nota2 = float(nota2)
-nota3 = float(nota3)
+nota1 = float(input("Digite a sua nota da primeira unidade:"))
+nota2 = float(input("Digite a sua nota da segunda unidade:"))
+nota3 = float(input("Digite a sua nota da terceira unidade:"))
 soma_notas = nota1 + nota2 + nota3
 media = soma_notas/3
 
